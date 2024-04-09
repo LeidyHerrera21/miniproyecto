a2060120bb7d89c003c8ed6aebf41841be187b8f
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2024-04-08 20:52:46 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,45 @@ document.getElementById("arrow").addEventListener("click", function() {
 // Recibo //
 
 document.getElementById("agregarItem").addEventListener("click", function() {
-    var item = prompt("Ingrese el nombre del producto:");
-    var precio = parseFloat(prompt("Ingrese el precio del producto:"));
-
-    if (item && precio) {
-        var listItem = document.createElement("li");
-        listItem.textContent = item + " - $" + precio.toFixed(2);
-        listItem.dataset.precio = precio;
-
-        var eliminarBtn = document.createElement("button");
-        eliminarBtn.textContent = "Eliminar";
-        eliminarBtn.addEventListener("click", function() {
-            listItem.parentNode.removeChild(listItem);
-            calcularTotal();
-        });
-
-        listItem.appendChild(eliminarBtn);
-        document.getElementById("items").appendChild(listItem);
+    var nombre = prompt("Ingrese el nombre de la comida:");
+    var precio = parseFloat(prompt("Ingrese el precio de la comida:"));
+    
+    if (nombre && precio) {
+        agregarItem(nombre, precio);
         calcularTotal();
     }
 });
 
+function agregarItem(nombre, precio) {
+    var itemDiv = document.createElement("div");
+    itemDiv.classList.add("item");
+
+    var nombreSpan = document.createElement("span");
+    nombreSpan.classList.add("nombre");
+    nombreSpan.textContent = nombre;
+    itemDiv.appendChild(nombreSpan);
+
+    var precioSpan = document.createElement("span");
+    precioSpan.textContent = "$" + precio.toFixed(2);
+    itemDiv.appendChild(precioSpan);
+
+    var eliminarBtn = document.createElement("button");
+    eliminarBtn.textContent = "Eliminar";
+    eliminarBtn.addEventListener("click", function() {
+        itemDiv.parentNode.removeChild(itemDiv);
+        calcularTotal();
+    });
+    itemDiv.appendChild(eliminarBtn);
+
+    document.getElementById("items").appendChild(itemDiv);
+}
+
 function calcularTotal() {
     var total = 0;
-    var items = document.querySelectorAll("#items li");
+    var items = document.querySelectorAll(".item span:nth-child(3)");
 
     items.forEach(function(item) {
-        total += parseFloat(item.dataset.precio);
+        total += parseFloat(item.textContent.slice(1)); // Elimina el signo $ y convierte a float
     });
 
     document.getElementById("totalAmount").textContent = total.toFixed(2);
