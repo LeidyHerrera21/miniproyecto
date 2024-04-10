45b950daf8968a945d6374a910a4437a9e493f77
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2024-04-10 18:48:00 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,46 +5,36 @@ document.getElementById("arrow").addEventListener("click", function() {
 // Recibo //
 
 document.getElementById("agregarItem").addEventListener("click", function() {
-    var nombre = prompt("Ingrese el nombre de la comida:");
-    var precio = parseFloat(prompt("Ingrese el precio de la comida:"));
-    
-    if (nombre && precio) {
-        agregarItem(nombre, precio);
+    var item = prompt("Ingrese el nombre del producto:");
+    var precio = parseFloat(prompt("Ingrese el precio del producto:"));
+
+    if (item && precio) {
+        var newItem = document.createElement("div");
+        newItem.classList.add("item");
+        newItem.textContent = item + " - $" + precio.toFixed(2);
+
+        var eliminarBtn = document.createElement("button");
+        eliminarBtn.textContent = "Eliminar";
+        eliminarBtn.addEventListener("click", function() {
+            newItem.parentNode.removeChild(newItem);
+            calcularTotal();
+        });
+
+        newItem.appendChild(eliminarBtn);
+        document.getElementById("items").appendChild(newItem);
         calcularTotal();
     }
 });
 
-function agregarItem(nombre, precio) {
-    var itemDiv = document.createElement("div");
-    itemDiv.classList.add("item");
-
-    var nombreSpan = document.createElement("span");
-    nombreSpan.classList.add("nombre");
-    nombreSpan.textContent = nombre;
-    itemDiv.appendChild(nombreSpan);
-
-    var precioSpan = document.createElement("span");
-    precioSpan.textContent = "$" + precio.toFixed(2);
-    itemDiv.appendChild(precioSpan);
-
-    var eliminarBtn = document.createElement("button");
-    eliminarBtn.textContent = "Eliminar";
-    eliminarBtn.addEventListener("click", function() {
-        itemDiv.parentNode.removeChild(itemDiv);
-        calcularTotal();
-    });
-    itemDiv.appendChild(eliminarBtn);
-
-    document.getElementById("items").appendChild(itemDiv);
-}
-
 function calcularTotal() {
     var total = 0;
-    var items = document.querySelectorAll(".item span:nth-child(3)");
+    var items = document.querySelectorAll(".item");
 
     items.forEach(function(item) {
-        total += parseFloat(item.textContent.slice(1)); // Elimina el signo $ y convierte a float
+        var precio = parseFloat(item.textContent.split(" - $")[1]);
+        total += precio;
     });
 
     document.getElementById("totalAmount").textContent = total.toFixed(2);
 }
+
